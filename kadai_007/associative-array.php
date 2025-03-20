76178bcb629f3047efd221d9fbaf23a8160c5883
sakisaki22 <harasaki2288@gmail.com>
2025-03-20 20:24:00 +0900

    kadai_007をアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>kadai_007</title>
+    </head>
+    <body>
+        <p>
+            <?php
+            $product_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+            print_r($product_data)
+            ?>
+        </p>
+    </body>
+</html>
